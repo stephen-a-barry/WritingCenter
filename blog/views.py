816838cf9d9a62ac636a00822646aac73f79e090
stephen-andrew-barry <stephen.andrew.barry@gmail.com>
2017-01-23 15:35:25 -0800

    added a blog detail file

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Post
 
@@ -8,3 +9,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', context={
         'posts':posts,
     })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
