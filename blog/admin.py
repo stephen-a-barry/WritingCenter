d9f744aa6941eff2fe56b63162d85df3024b7286
stephen-andrew-barry <stephen.andrew.barry@gmail.com>
2017-01-23 13:04:22 -0800

    updated model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+
 from .models import Post
 # Register your models here.
 # In order to see the model on the admin page, it must be registered here.   Import the class here, register the class
 # However, the interesting thing is 'blog' is not here--- but it is in settings under INSTALLED_APPS, and then also
 # when running manage.py startapp blog, makemigrations blog, migrate blog, and in apps.py (maybe most importantly)
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'status', 'author']
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,23 @@
-from __future__ import unicode_literals
-
 from django.db import models
-from django.utils import timezone
-# Create your models here.
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=1, choices=(
+        ('D', 'Draft'),
+        ('P', 'Published'),
+    ))
+
+    author = models.ForeignKey('auth.User',
+        related_name='posts')
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
-        return self.title      
+        return self.title
